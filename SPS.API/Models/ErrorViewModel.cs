b9c33bd1c651e5da40e29e5234e1c71ffa2648ac
navyanmekala <navyanmekala@gmail.com>
2025-10-24 19:01:52 +0530

    Add project files.

---
+++ SPS.API/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SPS.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
